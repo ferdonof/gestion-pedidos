6c833449a513cad29a5d7561c45a0c885c57c577
comit <comit@ferdonof-Aspire>
2021-02-05 21:06:19 -0300

    Modificaciones menores en la App principal

--- src/AppGestionPedidos.java
+++ src/AppGestionPedidos.java
@@ -1,8 +1,13 @@
+import entidades.Producto;
 
 public class AppGestionPedidos {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Producto clavosTecho = new Producto("A12NUM55", "Clavos de techo");
+		clavosTecho.setStock(500.0);
+		clavosTecho.setPrecio(1.0);
+
+		
 	}
 
 }
